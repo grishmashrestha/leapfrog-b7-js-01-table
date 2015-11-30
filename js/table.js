3059280998ba4219adcc8729762bd5e057bb570c
grishmashrestha <grishmashrestha@gmail.com>
2015-11-30 23:12:46 +0545

    created table and its header

--- js/table.js
+++ js/table.js
@@ -24,3 +24,19 @@ var studentDeets = [
     name: "Grumpy Cat"
   }
 ];
+
+// creating table elements
+var tbl = document.createElement('table');
+var tr = document.createElement('tr');
+var th1 = document.createElement('th'); //header 1 of the table
+var th2 = document.createElement('th'); //header 2 of the table
+
+//writing heading for the table and appending to the tr and then the table itself
+
+var th1Node = document.createTextNode('Roll No.');
+var th2Node = document.createTextNode('Name');
+th1.appendChild(th1Node);
+th2.appendChild(th2Node);
+tr.appendChild(th1);
+tr.appendChild(th2);
+tbl.appendChild(tr);
