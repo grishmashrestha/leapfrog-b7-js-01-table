14c5443734c2a8c20ad24251494dde0f0912eafa
grishmashrestha <grishmashrestha@gmail.com>
2015-11-30 22:58:25 +0545

    created table.js file with several data in studentDeets

---
+++ js/table.js
@@ -0,0 +1,26 @@
+var studentDeets = [
+  {
+    roll: 1,
+    name: "Success Kid"
+  },
+  {
+    roll: 2,
+    name: "Nyaan Cat"
+  },
+  {
+    roll: 3,
+    name: "Good Guy Greg"
+  },
+  {
+    roll: 4,
+    name: "Rage Guy"
+  },
+  {
+    roll: 5,
+    name: "Doge Such Wow"
+  },
+  {
+    roll: 6,
+    name: "Grumpy Cat"
+  }
+];
