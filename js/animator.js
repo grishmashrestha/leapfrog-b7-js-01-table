8ca4226bcf0cb94a9f1947ada658bd3fb9a6185a
romitamgai <rom_amgai@hotmail.com>
2015-12-02 14:55:49 +0545

    slider files initially added

---
+++ js/animator.js
@@ -0,0 +1,49 @@
+function Animator(element) {
+	this.el = element;
+
+	this.animate = function(cssProperty, value, duration) {
+		var style = window.getComputedStyle(element);
+		var initial = style.getPropertyValue(cssProperty);
+		initial = parseInt(initial);
+		console.log("initial",initial);
+		var slides = element.children;
+		var step = value / (duration / 50);
+
+		var counter = 0;
+		var intervalId = setInterval(function() {
+			counter++;
+			var current = step * counter;
+			console.log("current",current);
+			// element.innerHTML = current;
+			element.style[cssProperty] = (current) + initial + 'px';
+			console.log(element.style[cssProperty]);
+			if (counter >= duration/50)
+				clearInterval(intervalId);
+		
+		}, 50);
+	}
+
+	this.animateBatch = function(properties, duration) {
+		for (var x in properties) {
+			console.log(x, properties[x]);
+		}
+	}
+
+	// should stop the animation in current position
+	this.stop = function() {
+
+	}
+
+	// should stop the animation and element's properties should be at "end" value
+	this.finish = function() {
+
+	}
+
+	this.hitTest = function(singleElement) {
+
+	}
+
+	this.hitTestBatch = function(elements) {
+
+	}
+}
