1c6578112452ed72c5ea62ade83c6af2fb057670
Grishma Shrestha <grishmashrestha@gmail.com>
2015-12-07 21:48:28 +0545

    created functions move baddies and checkKeyPressed in creator.js

--- js/creator.js
+++ js/creator.js
@@ -8,18 +8,35 @@ function Creator() {
     var style = window.getComputedStyle(container);
     var initial = style.getPropertyValue("top");
     initial = parseInt(initial);
-    initial++;
-    container.style["top"] = initial + 'px'; 
+    container.style["top"] = initial + 1 + 'px'; 
   }
 
   this.createBaddies = function(){
     var noOfBaddies = Math.floor(Math.random() * 3) + 1;
     counterForBaddiesPosition++;
     console.log(noOfBaddies); 
-    for (i=0; i<noOfBaddies; i++) {
+    for (var i=0; i<noOfBaddies; i++) {
       allBaddies[i] = new BadBlock(i,counterForBaddiesPosition);
       allBaddies[i].create();
     }
+  }
+
+  this.moveBaddies = function() {
+    var len = allBaddies.length;
+    for(var i=0; i<len; i++){
+      allBaddies[i].move();
+    }
+  }
+
+  this.checkKeyPressed = function(e) {
+    var l = player.getLeft();
+    if (e.keyCode == "37") {
+      player.setLeft(l-4);
+    }
+    else if (e.keyCode == "39") {
+      player.setLeft(l+4);
+    }
+    player.move();
   }
 
   this.getAllBaddie = function() {
