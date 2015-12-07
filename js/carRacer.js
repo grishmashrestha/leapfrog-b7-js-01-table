e1c94f9fdf9f676e6e3f22e10b35285e71d3b1ed
Grishma Shrestha <grishmashrestha@gmail.com>
2015-12-07 21:48:54 +0545

    added eventListener for key press

--- js/carRacer.js
+++ js/carRacer.js
@@ -1,19 +1,22 @@
-var counter = 801;
+var counter = 800;
 var creator = new Creator();
 
 creator.createPlayer();
+addEventListener("keydown", creator.checkKeyPressed, false);
+
 setInterval(function(){
   gameloop();
-}, 5);
+}, 1);
 
 var gameloop = function() {
   creator.moveBackground();
-  if (counter > 800) {
+  if (counter == 800) {
     creator.createBaddies();
     counter = 0;
   }
-  
   creator.moveBaddies();
   counter ++;
   // creator.checkPlayerForCollision();
 }
+
+
